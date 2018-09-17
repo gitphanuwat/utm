4ee6718549487637f91d595f25dae67f876afd26
gitphanuwat <31411044+gitphanuwat@users.noreply.github.com>
2018-09-17 15:29:12 +0700

    กด

--- summary_data.php
+++ summary_data.php
@@ -295,49 +295,49 @@ if($_GET["action"]=="loadamp"){
 				   $idyear=$row[0];
 		   }
 
-		echo "<div class='chart' id='bar-chart-action' style='height: 250px;''></div> ";
-		echo "<script type='text/javascript'>";
-			echo "var bar = new Morris.Bar({";
-				echo "element: 'bar-chart-action',";
-				echo "resize: true,";
-				echo "data: [";
-				$sumb_trunk=0;
-				$sume_trunk=0;
-				$i=0;
-				$sql="select * from tb_amphur order by idamphur";
-				$result=mysqli_query($connect,$sql);
-				while($row=mysqli_fetch_array($result)){
-					$b_trunk=0;
-					$e_trunk=0;
+			 echo "<div class='chart' id='bar-chart-action2' style='height: 250px;''></div> ";
+	 		echo "<script type='text/javascript'>";
+	 			echo "var bar = new Morris.Bar({";
+	 				echo "element: 'bar-chart-action2',";
+	 				echo "resize: true,";
+	 				echo "data: [";
+	 				$sumb_trunk=0;
+	 				$sume_trunk=0;
+	 				$i=0;
+	 				$sql="select * from tb_tambon order by idtambon";
+	 				$result=mysqli_query($connect,$sql);
+	 				while($row=mysqli_fetch_array($result)){
+	 					$b_trunk=0;
+	 					$e_trunk=0;
 
-					$sql1="select iduser from tb_user where idamphur = $row[0]";
-					$result1=mysqli_query($connect,$sql1);
-					while($row1=mysqli_fetch_array($result1)){
+	 					$sql1="select iduser from tb_user where idtambon = $row[0]";
+	 					$result1=mysqli_query($connect,$sql1);
+	 					while($row1=mysqli_fetch_array($result1)){
 
-						$sql2="select tb_durian.iddurian, tb_durian.b_trunk, tb_durian.e_trunk from tb_durian, tb_plot where tb_plot.iduser = $row1[0] and tb_durian.idyear=$idyear";
-						$result2=mysqli_query($connect,$sql2);
-						while($row2=mysqli_fetch_array($result2)){
-							//$summoo=$summoo+$qmoo;
-							@$b_trunk = $b_trunk+$row2[1];
-							@$e_trunk = $e_trunk+$row2[2];
-						}//durian
-					}//user
-					$sumb_trunk = $sumb_trunk+$b_trunk;
-					$sume_trunk = $sume_trunk+$e_trunk;
+	 						$sql2="select tb_durian.iddurian, tb_durian.b_trunk, tb_durian.e_trunk from tb_durian, tb_plot where tb_durian.idplot=tb_plot.idplot and tb_plot.iduser = $row1[0] and tb_durian.idyear=$idyear";
+	 						$result2=mysqli_query($connect,$sql2);
+	 						while($row2=mysqli_fetch_array($result2)){
+	 							//$summoo=$summoo+$qmoo;
+	 							$b_trunk = $b_trunk+$row2[1];
+	 							$e_trunk = $e_trunk+$row2[2];
+	 						}//durian
+	 					}//user
+	 					$sumb_trunk = $sumb_trunk+$b_trunk;
+	 					$sume_trunk = $sume_trunk+$e_trunk;
 
-					echo "{y: '$row[1]', a: $b_trunk, b: $e_trunk },";
-					$i++;
-		 		}//amphur
-		 		echo "],";
-		 		echo "barColors: ['#00A65A', '#F39C11' ],";
-		 		echo "gridLineColor: '#666666',";
-		 		echo "gridTextColor: '#000000',";
-                echo "xkey: 'y',";
-                echo "ykeys: ['a', 'b' ],";
-                echo "labels: ['ต้นที่ปลูก', 'ต้นที่ให้ผลผลิต' ],";
-                echo "hideHover: 'auto'";
-		 	echo "});";
-		echo "</script>";
+	 					echo "{y: '$row[2]', a: $b_trunk, b: $e_trunk },";
+	 					$i++;
+	 		 		}//tambon
+	 		 		echo "],";
+	 		 		echo "barColors: ['#00A65A', '#F39C11' ],";
+	 		 		echo "gridLineColor: '#666666',";
+	 		 		echo "gridTextColor: '#000000',";
+	                 echo "xkey: 'y',";
+	                 echo "ykeys: ['a', 'b' ],";
+	                 echo "labels: ['จำนวนต้นที่ปลูก', 'จำนวนต้นที่ให้ผลผลิต' ],";
+	                 echo "hideHover: 'auto'";
+	 		 	echo "});";
+	 		echo "</script>";
 
 		exit();
 	}
